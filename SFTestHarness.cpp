edec02f8c5c15b204ea8370fff9ed04c018ee25d
rickghome <rickghome@gmail.com>
2018-02-12 21:10:03 -0800

    some basic boilerplate files

---
+++ SFTestHarness.cpp
@@ -0,0 +1,39 @@
+//
+//  SFTestHarness.cpp
+//  ECE180-Assignment3
+//
+//  Created by rick gessner on 2/12/18.
+//
+
+#include "SFTestHarness.hpp"
+
+using SoftwareFoundations::SFTestHarness;
+
+int SFTestHarness::runDateTests() {
+  return 0;
+}
+
+int SFTestHarness::runTimeTests() {
+  return 0;
+}
+
+int SFTestHarness::runDateTimeTests() {
+  return 0;
+}
+
+int SFTestHarness::runIntervalTests() {
+  return 0;
+}
+
+int SFTestHarness::runTimezoneTests() {
+  return 0;
+}
+
+int SFTestHarness::runTests() {
+  int theResult = runDateTests();
+  theResult+=runTimeTests();
+  theResult+=runDateTimeTests();
+  theResult+=runIntervalTests();
+  theResult+=runTimezoneTests();
+  return theResult;
+}
---
+++ SFTestHarness.hpp
@@ -0,0 +1,26 @@
+//
+//  SFTestHarness.hpp
+//  ECE180-Assignment3
+//
+//  Created by rick gessner on 2/12/18.
+//  Copyright © 2018 rick gessner. All rights reserved.
+//
+
+#ifndef SFTestHarness_hpp
+#define SFTestHarness_hpp
+
+namespace SoftwareFoundations {
+
+  class SFTestHarness {
+  public:
+    static int runTests();
+    static int runDateTests();
+    static int runTimeTests();
+    static int runDateTimeTests();
+    static int runIntervalTests();
+    static int runTimezoneTests();
+  };
+
+}
+
+#endif /* SFTestHarness_hpp */
